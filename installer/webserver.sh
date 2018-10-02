2e893ba351002b30024f8883647747634df99114
Aris Ripandi <ar.is@outlook.com>
2018-10-02 14:35:55 +0700

    Changed README

--- installer/webserver.sh
+++ installer/webserver.sh
@@ -32,14 +32,14 @@ curl -sS https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 #-----------------------------------------------------------------------------------------
 debconf-set-selections <<< "mysql-server mysql-server/root_password password `cat /tmp/ecp_dbname`"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password `cat /tmp/ecp_dbpass`"
-apt -y install gcc make cmake build-essential whois nscd dh-autoreconf \
-binutils dnsutils resolvconf ftp zip unzip bsdtar rsync screen ca-certificates \
-software-properties-common debconf-utils screenfetch haveged nmap nikto sqlite3 \
-speedtest-cli xmlstarlet {libpng,libssl,libffi}-dev libarchive-tools gamin mcrypt \
-gettext imagemagick libimage-exiftool-perl optipng jpegoptim php-{imagick,pear} \
-php7.2 php7.2-{common,cli,cgi,fpm,mbstring,opcache,gmp,readlinezip,sqlite3,intl} \
-php7.2-{bcmath,json,xml,xmlrpc,mysql,pgsql,imap,gd,curl,zip} nginx composer nodejs \
-letsencrypt mariadb-{server,client}
+apt -y install gcc make cmake build-essential whois nscd dh-autoreconf binutils \
+dnsutils resolvconf ftp zip unzip bsdtar rsync screen screenfetch ca-certificates \
+software-properties-common debconf-utils haveged nmap nikto sqlite3 speedtest-cli \
+xmlstarlet {libpng,libssl,libffi}-dev libarchive-tools gamin mcrypt imagemagick \
+gettext libimage-exiftool-perl optipng jpegoptim php-{imagick,pear} php7.2 \
+php7.2-{common,cli,cgi,fpm,mbstring,opcache,gmp,readlinezip,sqlite3,intl} \
+php7.2-{bcmath,json,xml,xmlrpc,mysql,pgsql,imap,gd,curl,zip} nginx composer \
+nodejs letsencrypt mariadb-{server,client}
 
 # Extra Packages
 curl -L# https://git.io/vN3Ff -o /usr/bin/wp ; chmod a+x /usr/bin/wp
