6610de8ae1c73977869a283c652b62d572b46949
Aris Ripandi <ar.is@outlook.com>
2018-10-02 15:34:44 +0700

    Changed repo

--- installer/repositories.sh
+++ installer/repositories.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-PWD=$(dirname "$(readlink -f "$0")")
-PARENT=$(dirname $PWD)
-
 if [[ $EUID -ne 0 ]]; then echo -e 'This script must be run as root' ; exit 1 ; fi
 
 # Get Country Code
@@ -13,15 +10,13 @@ country=`cat /tmp/country`
 if [ "$country" == "ID" ] ; then
   echo "deb http://kebo.pens.ac.id/debian `lsb_release -cs` main contrib non-free" > /etc/apt/sources.list
   echo "deb http://kebo.pens.ac.id/debian `lsb_release -cs`-updates main contrib non-free" >> /etc/apt/sources.list
-  echo "deb http://kebo.pens.ac.id/debian-security `lsb_release -cs`/updates main contrib non-free" >> /etc/apt/sources.list
 elif [ "$country" == "SG" ] ; then
   echo "deb http://sgp1.mirrors.digitalocean.com/debian `lsb_release -cs` main contrib non-free" > /etc/apt/sources.list
   echo "deb http://sgp1.mirrors.digitalocean.com/debian `lsb_release -cs`-updates main contrib non-free" >> /etc/apt/sources.list
-  echo "deb http://cdn-fastly.deb.debian.org/debian-security `lsb_release -cs`/updates main contrib non-free" >> /etc/apt/sources.list
 else
   echo "deb http://debian-archive.trafficmanager.net/debian `lsb_release -cs` main contrib non-free" > /etc/apt/sources.list
   echo "deb http://debian-archive.trafficmanager.net/debian `lsb_release -cs`-updates main contrib non-free" >> /etc/apt/sources.list
-  echo "deb http://debian-archive.trafficmanager.net/debian-security `lsb_release -cs`/updates main contrib non-free" >> /etc/apt/sources.list
 fi
+echo "deb http://debian-archive.trafficmanager.net/debian-security `lsb_release -cs`/updates main contrib non-free" >> /etc/apt/sources.list
 
 echo "Repository has been configured"
