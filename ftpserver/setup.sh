72ee22404fca02a2463e35d707baaa88ecaa2ec9
Aris Ripandi <ar.is@outlook.com>
2018-12-19 17:06:00 +0700

    Fixed FTP Server configuration

--- ftpserver/setup.sh
+++ ftpserver/setup.sh
@@ -20,14 +20,19 @@ iptables -A INPUT -p tcp -m tcp --dport 50000:50100 -j ACCEPT
 netfilter-persistent save
 netfilter-persistent reload
 
-openssl req -x509 -nodes -days 365 -newkey rsa:3072 -keyout /etc/ssl/private/proftpd.key -out /etc/ssl/certs/proftpd.crt -subj "/CN=$(hostname -f)"
+openssl req -x509 -nodes -days 365 -newkey rsa:3072 \
+ -keyout /etc/ssl/private/proftpd.key \
+ -out /etc/ssl/certs/proftpd.crt -subj "/CN=$(hostname -f)"
+
 chmod 0600 /etc/ssl/private/proftpd.key
 chmod 0640 /etc/ssl/private/proftpd.key
 
-rm -fr /etc/proftpd
-cp -r $PWD/config/proftpd /etc
+rm -fr /etc/proftpd/*
+cp -r $PWD/ftpserver/config/* /etc/proftpd/.
 chown -R root: /etc/proftpd
+
 sed -i "s/DB_NAME/$(cat /tmp/ecp_dbname)/"  /etc/proftpd/conf.d/sql.conf
 sed -i "s/DB_PASS/$(cat /tmp/ecp_dbpass)/"  /etc/proftpd/conf.d/sql.conf
 sed -i "s/DB_HOST/$(cat /tmp/db_bindaddr)/" /etc/proftpd/conf.d/sql.conf
+
 systemctl restart proftpd
