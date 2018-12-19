683b509f59aba8e5f37ed61422f876c89a8c7bcf
Tim Ruhay <timruhay@gmail.com>
2018-12-19 22:10:07 +0700

    Fixing basic pakage script

--- system/basicpkg.sh
+++ system/basicpkg.sh
@@ -2,30 +2,33 @@
 
 if [[ $EUID -ne 0 ]]; then echo -e 'This script must be run as root' ; exit 1 ; fi
 
-GetBin() {
-    curl -L# $1 -o $2
-    chmod a+x $2
-}
+echo -e "\nUpdating packages..."
+apt update -qq
 
-apt update && apt full-upgrade -yqq
+echo -e "\nUpgrading packages..."
+apt full-upgrade -yqq
 
+echo -e "\nInstalling basic packages..."
 apt install -yqq sudo nano figlet elinks pwgen curl crudini lsof ntp \
 ntpdate whois perl dirmngr software-properties-common debconf-utils \
 apt-transport-https gcc make cmake build-essential binutils dnsutils \
 nscd dh-autoreconf ftp zip unzip bsdtar pv rsync screen screenfetch \
 ca-certificates resolvconf
 
+echo -e "\nRemoving unnecessary packages..."
 apt autoremove -y
 
-echo -e "Downloading extra utilities..."
-GetBin https://semut.org/gdrive /usr/bin/gdrive
-GetBin https://dl.eff.org/certbot-auto /usr/bin/certbot
-GetBin https://git.io/vN3Ff /usr/bin/wp
-GetBin https://git.io/fAFyN /usr/bin/phpcs
-GetBin https://git.io/fAFyb /usr/bin/phpcbf
-GetBin https://cs.sensiolabs.org/download/php-cs-fixer-v2.phar /usr/bin/php-cs-fixer
+echo -e "\nDownloading extra utilities..."
 
-echo -e "Downloading Diffie-Hellman Parameter..."
+curl -L# "https://dl.eff.org/certbot-auto" -o /usr/local/bin/certbot
+curl -L# "https://git.io/vN3Ff" -o /usr/local/bin/wp
+curl -L# "https://git.io/fAFyN" -o /usr/local/bin/phpcs
+curl -L# "https://git.io/fAFyb" -o /usr/local/bin/phpcbf
+curl -L# "https://cs.sensiolabs.org/download/php-cs-fixer-v2.phar" -o /usr/local/bin/php-cs-fixer
+curl -L# "https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download" -o /usr/local/bin/gdrive
+
+echo -e "\nDownloading Diffie-Hellman Parameter..."
+
 curl -L# https://2ton.com.au/dhparam/4096/ssh -o /etc/ssh/moduli
 curl -L# https://2ton.com.au/dhparam/2048 -o /etc/ssl/certs/dhparam-2048.pem
 curl -L# https://2ton.com.au/dhparam/3072 -o /etc/ssl/certs/dhparam-3072.pem
