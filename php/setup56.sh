3c7a5e040383eb89598182cc5fc996d3b98f9135
Tim Ruhay <timruhay@gmail.com>
2018-12-26 18:45:33 +0700

    Fix php installation

--- php/setup56.sh
+++ php/setup56.sh
@@ -2,8 +2,8 @@
 
 if [[ $EUID -ne 0 ]]; then echo -e 'This script must be run as root' ; exit 1 ; fi
 
-apt -y install composer php5.6 php5.6-{common,cli,cgi,fpm,mbstring,opcache,xmlrpc,gmp} \
-php5.6-{bcmath,zip,sqlite3,intl,json,xml,imap,gd,curl,readline,zip,mysql,pgsql}
+apt update ; apt -y install composer php5.6 php5.6-{common,cli,cgi,fpm,mbstring,opcache} \
+php5.6-{bcmath,zip,sqlite3,intl,json,xml,imap,gd,curl,readline,zip,mysql,pgsql,xmlrpc,gmp}
 
 crudini --set /etc/php/5.6/fpm/php-fpm.conf  'www' 'listen' '/var/run/php/php5.6-fpm.sock'
 systemctl restart php5.6-fpm
--- php/setup73.sh
+++ php/setup73.sh
@@ -2,8 +2,8 @@
 
 if [[ $EUID -ne 0 ]]; then echo -e 'This script must be run as root' ; exit 1 ; fi
 
-apt -y install composer php7.3 php7.3-{common,cli,cgi,fpm,bcmath,mbstring,opcache,json} \
-php7.3-{gmp,imagick,readline,zip,sqlite3,intl,xml,xmlrpc,curl,zip,mysql,pgsql,imap,gd}
+apt update ; apt -y install composer php7.3 php7.3-{common,cli,cgi,fpm,bcmath,mbstring,json} \
+php7.3-{gmp,imagick,readline,zip,sqlite3,intl,xml,xmlrpc,curl,zip,mysql,pgsql,imap,gd,opcache}
 
 crudini --set /etc/php/7.3/fpm/php-fpm.conf  'www' 'listen' '/var/run/php/php7.3-fpm.sock'
 systemctl restart php7.3-fpm
