4b8d3a611b10654b027c7d54d9d57fa7e30b4137
Tim Ruhay <timruhay@gmail.com>
2018-12-26 18:36:43 +0700

    Removing flask installation

--- python/setup.sh
+++ python/setup.sh
@@ -3,7 +3,6 @@
 if [[ $EUID -ne 0 ]]; then echo -e 'This script must be run as root' ; exit 1 ; fi
 
 apt -y install {python,python3}-{dev,virtualenv,pip,setuptools,gunicorn,mysqldb} \
-supervisor {python,python3}-{flaskext.wtf,flask-{migrate,restful,sqlalchemy,bcrypt}} \
-python-{m2crypto,configparser} gunicorn gunicorn3
+supervisor python-{m2crypto,configparser} gunicorn gunicorn3
 
 source $ROOT/python/configure.sh
