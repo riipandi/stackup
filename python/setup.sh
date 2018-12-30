fda4ecd31ba88900f512f1c06b604a360ceffd01
Tim Ruhay <timruhay@gmail.com>
2018-12-30 17:38:10 +0700

    Fix some python script

--- python/setup.sh
+++ python/setup.sh
@@ -5,4 +5,4 @@ if [[ $EUID -ne 0 ]]; then echo -e 'This script must be run as root' ; exit 1 ; 
 apt -y install {python,python3}-{dev,virtualenv,pip,setuptools,gunicorn,mysqldb} \
 supervisor python-{m2crypto,configparser} gunicorn gunicorn3
 
-source $ROOT/python/configure.sh
+source $ROOT/python/configure.sh 3.5
