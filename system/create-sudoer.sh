77c780b4aca71e4611016ffdc56c3cbf05371c1c
Tim Ruhay <timruhay@gmail.com>
2019-01-12 00:31:17 +0700

    Added user creation snippet

--- system/create-sudoer.sh
+++ system/create-sudoer.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 if [[ $EUID -ne 0 ]]; then echo 'This script must be run as root' ; exit 1 ; fi
 
-PWD=$(dirname "$(readlink -f "$0")")
-PARENT=$(dirname "$PWD")
+NO='\033[0;33m'
+OK='\033[0;32m'
+NC='\033[0m'
 
-# [[ $(cat /etc/passwd | grep -c "$user") -eq 1 ]] || useradd xxxxxx
-
-
-CreateNewUser() {
-    while true; do
-        echo
-        read -sp "Enter new user password              : " userpass1
-        [ "$userpass1" == "" ] && CreateNewUser
-        echo
-        read -sp "Enter new user password (again)      : " userpass2
-        [ "$userpass1" = "$userpass2" ] && break
-    done
-    echo
-}
-
-read -ep "Create a new user?            yes/no : " -i "yes" createuser
-if [[ "${createuser,,}" =~ ^(yes|y)$ ]] ; then
-    read -ep "Enter new user fullname              : " -i "Admin Sistem" fullname
-    read -ep "Enter new user username              : " -i "admin" username
-    CreateNewUser
-    useradd -mg sudo -s `which bash` $username -c "$fullname" -p `openssl passwd -1 "$userpass1"`
-    SetConfigSetup setup create_user no
-fi
+while true; do
+    read -ep "Enter username for this user       : " -i "admin" username
+    read -ep "Enter new user real name          : " -i "Admin Sistem" fullname
+    egrep "^$username" /etc/passwd >/dev/null
+    if [ $? -eq 0 ]; then
+        echo -e "${NO}User $username already exists!${NC}"
+    else
+        while true; do
+            read -sp "Enter password for new user       : " password
+            if [ "$password" == "" ]; then
+                echo -e "${NO}\nPlease enter user password!${NC}"
+            else
+                password="$password" && break && echo
+            fi
+        done
+        # Create new user
+        pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+        useradd -mg sudo -s `which bash` $username -c "$fullname" -p $pass
+        if [ $? -eq 0 ] ; then
+            HOMEDIR=$(eval echo "~$username")
+            mkdir -p $HOMEDIR/.ssh ; chmod 0700 $_
+            touch $HOMEDIR/.ssh/id_rsa ; chmod 0600 $_
+            touch $HOMEDIR/.ssh/id_rsa.pub ; chmod 0600 $_
+            touch $HOMEDIR/.ssh/authorized_keys ; chmod 0600 $_
+            chown -R $username: $HOMEDIR/.ssh
+            echo -e "${OK}\nUser ${NO}$username${NC} ${OK}has been added to system!${NC}"
+        else
+            echo -e "${NO}Failed to add a user!${NC}"
+        fi
+        break
+    fi
+done
