8dcb1b40a1fc55f7628abb46537a702db4adc113
Aris Ripandi <ar.is@outlook.com>
2019-05-27 04:17:28 +0700

    Change FTP server to Pure-FTPd

--- installer/setup-ftp-server.sh
+++ installer/setup-ftp-server.sh
@@ -10,16 +10,21 @@ FTP_DB_PASS=`pwgen -1 16`
 USER_PASS=$(echo "{md5}"`/bin/echo -n "secret" | openssl dgst -binary -md5 | openssl enc -base64`)
 
 #-----------------------------------------------------------------------------------------
-mysql -e "CREATE DATABASE IF NOT EXISTS stackup_ftp"
-mysql -e "CREATE USER IF NOT EXISTS 'stackup_ftp'@'127.0.0.1' IDENTIFIED BY '$FTP_DB_PASS'"
-mysql -e "GRANT ALL PRIVILEGES ON stackup_ftp.* TO 'stackup_ftp'@'127.0.0.1'"
-mysql -e "FLUSH PRIVILEGES"
+# mysql -uroot -e "DROP DATABASE IF EXISTS stackup_ftp"
+# mysql -uroot -e "DROP USER IF EXISTS 'stackup_ftp'@'127.0.0.1'"
 
-# Install ProFTPd
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS stackup_ftp"
+mysql -uroot -e "CREATE USER IF NOT EXISTS 'stackup_ftp'@'127.0.0.1' IDENTIFIED BY '$FTP_DB_PASS'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stackup_ftp.* TO 'stackup_ftp'@'127.0.0.1'"
+mysql -uroot -e "FLUSH PRIVILEGES"
+
+# Install Packages
 #-----------------------------------------------------------------------------------------
-apt update ; apt -y install proftpd-mod-mysql
+apt update ; apt -y install pure-ftpd-common pure-ftpd-mysql
 
-# Configure ProFTPd
+mysql -uroot stackup_ftp < $PWD/config/schemas/pureftpd.sql
+
+# Configure Packages
 #-----------------------------------------------------------------------------------------
 [[ $(cat /etc/group | grep -c webmaster) -eq 1 ]] || groupadd -g 2001 webmaster
 [[ $(cat /etc/passwd | grep -c webmaster) -eq 1 ]] || useradd -u 2001 -s /usr/sbin/nologin -d /bin/null -g webmaster webmaster
@@ -27,17 +32,17 @@ apt update ; apt -y install proftpd-mod-mysql
 chown -R webmaster:webmaster /var/www/public_ftp
 chmod -R 0755 /var/www/public_ftp
 
-rm -fr /etc/proftpd/*
-cp -r $PWD/config/proftpd/* /etc/proftpd/.
-mkdir -p /etc/proftpd/vhost.d
-chown -R root: /etc/proftpd
+rm -fr /etc/pure-ftpd/*
+cp -r $PWD/config/pure-ftpd/* /etc/pure-ftpd/.
+chown -R root: /etc/pure-ftpd
 
-mysql -uroot stackup_ftp < $PWD/config/schemas/ftpserver.sql
+sed -i "s/HOSTNAME/$(hostname -f)/" /etc/pure-ftpd/pure-ftpd.conf
+sed -i "s/DB_HOST/127.0.0.1/"       /etc/pure-ftpd/db-mysql.conf
+sed -i "s/DB_PORT/3306/"            /etc/pure-ftpd/db-mysql.conf
+sed -i "s/DB_NAME/stackup_ftp/"     /etc/pure-ftpd/db-mysql.conf
+sed -i "s/DB_USER/stackup_ftp/"     /etc/pure-ftpd/db-mysql.conf
+sed -i "s/DB_PASS/${FTP_DB_PASS}/"  /etc/pure-ftpd/db-mysql.conf
 
-sed -i "s/HOSTNAME/$(hostname -f)/" /etc/proftpd/tls.conf
-sed -i "s/DB_HOST/127.0.0.1/"       /etc/proftpd/sql.conf
-sed -i "s/DB_NAME/stackup_ftp/"     /etc/proftpd/sql.conf
-sed -i "s/DB_USER/stackup_ftp/"     /etc/proftpd/sql.conf
-sed -i "s/DB_PASS/${FTP_DB_PASS}/"  /etc/proftpd/sql.conf
-systemctl restart proftpd ; systemctl status -l proftpd
-# tail -f /var/log/proftpd/proftpd.log
+systemctl restart pure-ftpd-mysql
+systemctl status pure-ftpd-mysql
+# tail -f /var/log/syslog | grep ftpd
