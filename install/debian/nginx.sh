29341dd744c37834ef87d638e7dfdc418493f546
Aris Ripandi <ar.is@outlook.com>
2019-11-14 00:25:26 +0700

    Committed with Git-Upload

--- install/debian/nginx.sh
+++ install/debian/nginx.sh
@@ -14,7 +14,21 @@ echo -e "\n${BLUE}Installing Nginx...${NOCOLOR}"
 
 # Install packages
 #-----------------------------------------------------------------------------------------
-apt update -qq ; apt full-upgrade -yqq ; apt -yqq install xxxxxxxxxxxxxxxx
+curl -sS http://nginx.org/keys/nginx_signing.key | apt-key add - &>/dev/null
+cat > /etc/apt/sources.list.d/nginx.list <<EOF
+deb [arch=amd64] https://nginx.org/packages/mainline/debian `lsb_release -cs` nginx
+EOF
+
+apt update -qq ; apt full-upgrade -yqq
+apt -yqq install {libpng,libssl,libffi,libexpat1}-dev libarchive-tools libimage-exiftool-perl \
+libaugeas0 haveged gamin nginx augeas-lenses python-dev openssl
+
+# Download latest certbot
+echo -e "\n${BLUE}Downloading certbot and trusted certificates...${NOCOLOR}"
+curl -L# https://dl.eff.org/certbot-auto -o /usr/bin/certbot ; chmod a+x /usr/bin/certbot
+curl -L# https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem.txt -o /etc/ssl/certs/chain.pem
+curl -L# https://2ton.com.au/dhparam/4096 -o /etc/ssl/certs/dhparam-4096.pem
+curl -L# https://2ton.com.au/dhparam/2048 -o /etc/ssl/certs/dhparam-2048.pem
 
 # Configure packages
 #-----------------------------------------------------------------------------------------
